c42ce34e732639e308c6f8b12a6876115c8680b7
backsapce <backsapce@hotmail.com>
2020-12-16 03:55:52 +0300

    fixed bug with variables

--- 7_update-agent-vars.bat
+++ 7_update-agent-vars.bat
@@ -1,1 +1,2 @@
-docker exec agent-core sh -c "env | egrep -v '^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_=)' >> /etc/environment"
+docker exec agent-core sh -c ^ 
+"env | egrep -v '^(HOME=|USER=|MAIL=|LC_ALL=|LS_COLORS=|LANG=|HOSTNAME=|PWD=|TERM=|SHLVL=|LANGUAGE=|_=)' >> /etc/environment"
